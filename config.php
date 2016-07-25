9ed12f44ff32653172ae882287b2aedfa4dda92f
aleksander <elitelucifer@gmail.com>
2016-07-25 16:13:49 +0700

    Changed config.php for specifics of heroku

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
-<?php
+<   ?php
     $root_dir = __DIR__ . "/";
-    $root_url = !empty($_SERVER['HTTPS']) ? "https://" : "http://";
+    $root_url = "https://";
     $root_url .= $_SERVER['HTTP_HOST'] . "/";
     define("ROOT_URL", $root_url);
     define("ROOT_DIR", $root_dir);
