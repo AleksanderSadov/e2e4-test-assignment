47c609444a200302a6ce7a03e4dac00628f47f71
Aleksander Sadov <aleksander.sadov@office.e2e4gu.ru>
2016-08-22 18:25:14 +0700

    Удалил лишний метод получения комментариев  в модели CommentTable. Убрал ненужное указание названия таблицы в той же модели.

--- app/model/table/CommentsTable.php
+++ app/model/table/CommentsTable.php
@@ -3,23 +3,7 @@ class CommentsTable extends InitialTable
 {
     public function __construct() 
     {
-        $table_name = "comments";
-        $object_name = "Comment";
-        parent::__construct($table_name, $object_name);
-    }
-    
-    public function GetAllComments($topic_id)
-    {
-        $comments = parent::Select("*", "topic='$topic_id'", "date", "DESC");
-        
-        if (isset($comments) && !empty($comments))
-        {
-            return $comments;
-        }
-        else 
-        {
-            return "Нет комментариев";
-        }
+        parent::__construct();
     }
 }
 ?>
