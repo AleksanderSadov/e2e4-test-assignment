b7394056bbe58c7c05f8198708f612f7639ab655
aleksander <elitelucifer@gmail.com>
2016-09-01 11:37:42 +0700

    Добавил папку public для хранения доступных для пользователей файлов. Перенес фукнцию автозагрузки классов в корневой index.php.

---
+++ app/public/index.php
@@ -0,0 +1,15 @@
+<?php
+    // Загрузка конфига
+    if (file_exists("app/config/config.php"))
+    {
+        include_once ("app/config/config.php");
+    }
+    else
+    {
+        die ("Не удалось загрузисть конфиг файл");
+    }
+    
+    // Определение url запроса
+    $controller_front = new FrontController("MessagesController", "Index");
+    $controller_front->Dispatch();
+
--- index.php
+++ index.php
@@ -1,4 +1,34 @@
 <?php
-    require "app/index.php";
-?>
+    // Автозагрузка классов
+    function my_autoloader($class_name)
+    {
+        // Директории поиска классов
+        $directories = array(
+            'core/controller/',
+            'core/model/database/',
+            'core/model/entity/',
+            'core/model/table/',
+            'core/view/',
+            
+            'app/controller/',
+            'app/model/database/',
+            'app/model/entity/',
+            'app/model/table/',
+            'app/view/',
+        );
+       
+        foreach($directories as $directory)
+        {
+            $path = $directory . $class_name . ".php";
+            if(file_exists($path))
+            {
+                require_once($path);
+                // Если класс найден прекращаем поиск
+                return;
+            }
+        }
+    }
+    spl_autoload_register("my_autoloader");
+    
+    require "app/public/index.php";
 
