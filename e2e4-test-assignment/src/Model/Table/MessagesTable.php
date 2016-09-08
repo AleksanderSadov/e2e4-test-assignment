04fa56c1ffc0c44a1b2de27932809b104856ff76
aleksander <elitelucifer@gmail.com>
2016-09-09 03:03:12 +0700

    Убрал вызовы var_dump.

--- e2e4-test-assignment/src/Model/Table/MessagesTable.php
+++ e2e4-test-assignment/src/Model/Table/MessagesTable.php
@@ -117,8 +117,6 @@ class MessagesTable extends Table
     {
         $dateFrom = (new \DateTime($options['dateFrom']))->format('Y-m-d 00:00:00');
         $dateTo = (new \DateTime($options['dateTo']))->format('Y-m-d 23:59:59');
-        var_dump($dateFrom);
-        var_dump($dateTo);
         return $query->where(['Messages.created BETWEEN :start AND :end'])
             ->bind(':start', $dateFrom, 'datetime')
             ->bind(':end', $dateTo, 'datetime');
